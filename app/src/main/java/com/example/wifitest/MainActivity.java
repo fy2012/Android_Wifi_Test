904f2d1e3087015f111774a58c7b4237722c0a34
Zifan Yang <yangzifan@gmail.com>
2019-09-21 14:45:40 +0800

    Complete WifiTester class

--- app/src/main/java/com/example/wifitest/MainActivity.java
+++ app/src/main/java/com/example/wifitest/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.wifitest;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
-import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -15,17 +13,11 @@ import android.os.Vibrator;
 
 public class MainActivity extends AppCompatActivity {
 
-    int speedinfo;
-    String units = "";
-    int strengthinfo;
-    int frequencyinfo;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //final TextView title = findViewById(R.id.title);
         final Button go = findViewById(R.id.check);
         final TextView strength = findViewById(R.id.strength);
         final TextView speed = findViewById(R.id.speed);
@@ -33,81 +25,47 @@ public class MainActivity extends AppCompatActivity {
         final TextView is_good = findViewById(R.id.is_good);
         final TextView is_2_4G = findViewById(R.id.is_2_4G);
 
+        final Button share = findViewById(R.id.share);
+
         final Vibrator z = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
         go.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                getInfo();
+            WifiTester wifiTester = new WifiTester((WifiManager) getSystemService(WIFI_SERVICE));
 
-                strength.setText("Strength: " + strengthinfo);
+            strength.setText("Strength: " + wifiTester.getStrength());
 
-                speed.setText("Speed: " + speedinfo + " " + units);
+            speed.setText("Speed: " + wifiTester.getSpeed() + " " + wifiTester.getUnit());
 
-                frequency.setText("Frequency: " + frequencyinfo + "");
+            frequency.setText("Frequency: " + wifiTester.getFrequency() + " Mhz");
 
-                if(isWifiGood())
-                    is_good.setText("Wifi is Good");
-                else
-                    is_good.setText("Wifi is not Good");
+            if(wifiTester.getStrength() > 1)
+                is_good.setText("Wifi is Good");
+            else
+                is_good.setText("Wifi is not Good");
 
-                if(is_2_4G())
-                    is_2_4G.setText("On 2.4G Wifi");
-                else
-                    is_2_4G.setText("Not on 2.4G Wifi");
+            if(wifiTester.getFrequency() < 2800)
+                is_2_4G.setText("On 2.4G Wifi");
+            else
+                is_2_4G.setText("Not on 2.4G Wifi");
 
-
-            // Vibrate for 500 milliseconds
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    z.vibrate(VibrationEffect.createOneShot(200, VibrationEffect.DEFAULT_AMPLITUDE));
-                } else {
-                    //deprecated in API 26
-                    z.vibrate(200);
-                }
+            // Vibrate for 200 milliseconds
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                z.vibrate(VibrationEffect.createOneShot(200, VibrationEffect.DEFAULT_AMPLITUDE));
+            } else {
+                //deprecated in API 26
+                z.vibrate(200);
+            }
             }
         });
-    }
 
-    private void getInfo(){
-        WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
-        WifiInfo info = wifiManager.getConnectionInfo();
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-            speedinfo = info.getLinkSpeed();
-            units = WifiInfo.LINK_SPEED_UNITS;
-            strengthinfo = WifiManager.calculateSignalLevel(info.getRssi(), 5);
-            frequencyinfo = info.getFrequency();
-    }
-
-    private boolean isWifiGood(){
-        int strength;
-
-        WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
-        WifiInfo info = wifiManager.getConnectionInfo();
-
-        //WIFI 信号强度分为5级
-        strength = WifiManager.calculateSignalLevel(info.getRssi(), 5);
-
-        //如果在1级以上，给正面反馈
-        if (strength > 1)
-            return true;
-        else
-            return false;
-    }
-
-    private boolean is_2_4G(){
-
-        int frequency;
-
-        WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
-        WifiInfo info = wifiManager.getConnectionInfo();
-
-        //检测Wifi信号频率
-        frequency = info.getFrequency();
-
-        //如果频率在范围之内，给正面反馈
-        if (frequency < 2800 && frequency > 2000)
-            return true;
-        else return false;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/wifitest/WifiTester.java
@@ -0,0 +1,62 @@
+package com.example.wifitest;
+
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+
+public class WifiTester {
+
+    protected WifiInfo wifiinfo;
+
+    private int speed;
+    private String unit;
+    private int strength;
+    private int frequency;
+
+    public WifiTester(WifiManager wifiManager){
+        setWifiInfo(wifiManager.getConnectionInfo());
+        setStrength(WifiManager.calculateSignalLevel(wifiinfo.getRssi(), 5));
+        setSpeed(wifiinfo.getLinkSpeed());
+        setUnit(wifiinfo.LINK_SPEED_UNITS);
+        setFrequency(wifiinfo.getFrequency());
+    }
+
+    public void setWifiInfo(WifiInfo info) {
+        this.wifiinfo = info;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+
+    public WifiInfo getWifiInfo() {
+        return wifiinfo;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+}
